3d1e01a0ea07663b659e21eb20437a2c09b5b378
ndragon798 <ndragon798@yahoo.com>
2015-12-13 03:05:35 -0500

    Version 1.0
    Version 1.0
    Scrape magnet links from the piratebay

---
+++ Magnet Scraper/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.HelpProvider1 = New System.Windows.Forms.HelpProvider()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(597, 464)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(55, 26)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 470)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(92, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(97, 9)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(178, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.HorizontalScrollbar = True
+        Me.ListBox2.Location = New System.Drawing.Point(12, 99)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.ScrollAlwaysVisible = True
+        Me.ListBox2.Size = New System.Drawing.Size(640, 264)
+        Me.ListBox2.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(534, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(118, 28)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Find Magnets"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ToolTip1
+        '
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(75, 38)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(36, 20)
+        Me.NumericUpDown1.TabIndex = 6
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(143, 64)
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(35, 20)
+        Me.NumericUpDown2.TabIndex = 7
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(75, 64)
+        Me.NumericUpDown3.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(36, 20)
+        Me.NumericUpDown3.TabIndex = 8
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.HelpProvider1.SetHelpString(Me.Button3, "This file will be exported as a txt file in the same directory as this program")
+        Me.Button3.Location = New System.Drawing.Point(534, 56)
+        Me.Button3.Name = "Button3"
+        Me.HelpProvider1.SetShowHelp(Me.Button3, True)
+        Me.Button3.Size = New System.Drawing.Size(118, 28)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Export"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Name Of Show"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Season"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Episodes"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(117, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(20, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "To"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(469, 71)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(55, 13)
+        Me.LinkLabel1.TabIndex = 14
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "My Github"
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(664, 378)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.HelpButton = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(680, 417)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(680, 417)
+        Me.Name = "Form1"
+        Me.Text = "Piratebay Magnet Finder"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents HelpProvider1 As System.Windows.Forms.HelpProvider
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ Magnet Scraper/Form1.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        MsgBox("Please Wait this can take a while if you have a large range")
+        Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create(TextBox1.Text)
+        Dim response As System.Net.HttpWebResponse = request.GetResponse
+
+        Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+
+        Dim rssourcecode As String = sr.ReadToEnd
+
+
+        'rssourcecode = Regex.Replace(rssourcecode, "<!(.|\s)*?>", "")
+        'href="magnet:?xt=urn:btih:52bda2d7e89b0a60931a5732e38768474aca8bff&amp;dn=Castle+2009+S02E01+HDTV+XviD-2HD+%5Beztv%5D&amp;tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&amp;tr=udp%3A%2F%2Fopen.demonii.com%3A1337&amp;tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&amp;tr=udp%3A%2F%2Fexodus.desync.com%3A6969"
+
+        '  Dim r = New Regex("magnet:?xt=urn:btih:.*")
+        'Dim r = New Regex("<a style=.*background-image: url.*>") href=.*
+        Dim r = New Regex("magnet:.*btih.*%3A6969")
+        Dim matches As MatchCollection = r.Matches(rssourcecode)
+        'If matches.Count.Equals(0) Then
+        'Else
+        '    ListBox2.Items.Add(matches(1))
+        'End If
+        Try
+            ListBox2.Items.Add(matches(1))
+
+        Catch Exc As System.ArgumentOutOfRangeException
+            MsgBox("You probably entered too many episodes or it just can't be found on thepiratebay.la. Just keep clicking OK")
+        End Try
+        'For Each url As Match In matches
+        '    ListBox1.Items.Add(url)
+        '    TextBox2.AppendText(url.ToString)
+        'Next
+        'For Each url As Match In matches
+        '    ListBox1.Items.Add(url)
+        '    'MsgBox(url)
+        'Next
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        'ListBox1.Items.Add("test")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox2.Text.Replace(" ", "%20")
+        Dim x As Integer
+        x = NumericUpDown1.Value
+        Dim xs As String
+
+        Dim y As Integer
+        y = NumericUpDown3.Value
+
+        Dim ys As String
+        Do While x <= NumericUpDown1.Value
+            Do While y <= NumericUpDown2.Value
+                If x <= 9 Then
+                    xs = "0" + x.ToString
+                Else
+                    xs = x.ToString
+                End If
+                If y <= 9 Then
+                    ys = "0" + y.ToString
+                Else
+                    ys = y.ToString
+                End If
+                TextBox1.Text() = "http://thepiratebay.la/lucky/" + TextBox2.Text + "%20S" + xs + "E" + ys + "/"
+                Button1.PerformClick()
+
+                y = y + 1
+                'Threading.Thread.Sleep(500)
+            Loop
+            x = x + 1
+        Loop
+        Button3.Enabled = True
+
+    End Sub
+
+    Private Sub ToolTip1_Popup(sender As Object, e As PopupEventArgs)
+
+    End Sub
+
+    Private Sub ToolTip1_Popup_1(sender As Object, e As PopupEventArgs) Handles ToolTip1.Popup
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ToolTip1.SetToolTip(TextBox2, "Set a TV Show Name such as Castle 2009")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim w As IO.StreamWriter
+        w = New IO.StreamWriter("export.txt")
+        For i = 0 To ListBox2.Items.Count - 1
+            w.WriteLine(ListBox2.Items.Item(i))
+        Next
+        w.Flush()
+        w.Close()
+    End Sub
+
+    Private Sub CreateHelpProvider()
+        Dim hlpProvider As HelpProvider
+        hlpProvider = New System.Windows.Forms.HelpProvider()
+        hlpProvider.SetShowHelp(Button3, "This file will be exported as a txt file in the same directory as this program")
+    End Sub
+
+    Private Sub NumericUpDown3_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown3.ValueChanged
+        If NumericUpDown3.Value > NumericUpDown2.Value Then
+            NumericUpDown3.Value = NumericUpDown2.Value
+        End If
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        If NumericUpDown3.Value > NumericUpDown2.Value Then
+            NumericUpDown3.Value = NumericUpDown2.Value
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://github.com/ndragon798")
+    End Sub
+End Class
